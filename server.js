615350e157a71f9cc51d66de795e09fe0b488867
chadmuro <chadmuro@gmail.com>
2021-07-25 16:38:10 +0900

    deploy to heroku

--- server.js
+++ server.js
@@ -7,18 +7,21 @@ mongoose.connect(process.env.DATABASE_URL, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true,
+	server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+	replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
 });
 const db = mongoose.connection;
 db.on('error', error => console.log(error));
 db.once('open', () => console.log('Connected to database'));
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
 const vocabularyRouter = require('./routes/vocabulary');
 app.use('/vocabulary', vocabularyRouter);
 
-app.listen(3000, () => {
-	console.log('Listening on port 3000');
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
 });
