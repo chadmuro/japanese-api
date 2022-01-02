891c7e2ab5dd72923ee51c904f75b3a610efb58b
chadmuro <chadmuro@gmail.com>
2022-01-02 12:33:38 +0900

    add updated database url

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.OLD_DATABASE_URL, {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
