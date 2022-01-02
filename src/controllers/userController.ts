7bc2511043699774c8bf9f62420528a5947a3d42
chadmuro <chadmuro@gmail.com>
2022-01-02 11:20:47 +0900

    add auth check route

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -49,7 +49,41 @@ const user_login = async (req: Request, res: Response) => {
   }
 };
 
+const user_check = async (req: Request, res: Response) => {
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader?.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'Not authorized' });
+
+    let username = '';
+
+    jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET as string,
+      (err, user) => {
+        console.log(user);
+        if (err) return res.status(403).json({ message: 'Not authorized' });
+        username = user?.username;
+      }
+    );
+
+    console.log(username);
+    const currentUser = await User.findOne({
+      username,
+    }).exec();
+    console.log(currentUser);
+    res.status(200).json({
+      username: currentUser.username,
+      role: currentUser.role,
+      accessToken: token,
+    });
+  } catch (err: any) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
 export default {
   user_post,
   user_login,
+  user_check,
 };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,9 +4,12 @@ import userController from '../controllers/userController';
 const router = express.Router();
 
 // CREATE NEW ONE
-router.post('/', userController.user_post);
+router.post('/signup', userController.user_post);
 
 // LOGIN EXISTING ONE
 router.post('/login', userController.user_login);
+
+// CHECK EXISTING ONE
+router.post('/check', userController.user_check);
 
 export default router;
